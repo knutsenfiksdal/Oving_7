7f16e661fc39cb514b750eb73e76f755446c88ed
Marius <marius@fiksdal.io>
2021-03-03 22:00:03 +0100

    Implemented unittest for name property

--- tests.py
+++ tests.py
@@ -1,1 +1,25 @@
-#Empty
+import unittest, math
+
+from tur_fixed import Tur
+
+tur1 = Tur("navn", 0, 10)
+
+class TestTur(unittest.TestCase):
+    def test_navn(self):
+        with self.assertRaises(TypeError):
+            tur1.navn = 1
+        with self.assertRaises(TypeError):
+            tur1.navn = list
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+"""
+Skriv enhetstester for egenskapen slutt-tidspunkt og metodene som er oppgitt over. 
+Lag turer med eksempelverdiene oppgitt over for å teste «hoydemeter» og «er_rundtur» metodene. 
+Du kan gjerne lage flere turer også. 
+Testen for slutt-tidspunkt skal sjekke at du får en exception hvis du prøver å lage en tur som slutter før den starter.
+"""
