f2436a6ebd46542311da4459279e572a7f854e3c
Marius <marius@fiksdal.io>
2021-03-09 11:33:17 +0100

    Implemented V2, should work now

--- tests.py
+++ tests.py
@@ -1,25 +1,85 @@
-import unittest, math
+import unittest
+from tur import Tur, Posisjon
 
-from tur_fixed import Tur
-
-tur1 = Tur("navn", 0, 10)
 
 class TestTur(unittest.TestCase):
-    def test_navn(self):
-        with self.assertRaises(TypeError):
-            tur1.navn = 1
-        with self.assertRaises(TypeError):
-            tur1.navn = list
+    def test_slutt_tidspunkt(self):
+        tur1 = Tur('tur', 0, 15)
+        tur2 = Tur('tur', 4, 6)
+        self.assertGreater(tur1.sluttidspunkt, tur1.starttidspunkt)
+        self.assertEqual(15, tur1.sluttidspunkt)
+        self.assertEqual(6, tur2.sluttidspunkt)
+        with self.assertRaises(ValueError):
+            tur2.sluttidspunkt = 3
+
+    def test_add_posisjon(self):
+        tur1 = Tur('Tur1', 0, 15)
+        punkt1 = Posisjon(2, 3, 2)
+        punkt2 = Posisjon(4, 4, 7)
+        tur1.add_posisjon(punkt1)
+        self.assertEqual(1, len(tur1.posisjoner))
+        self.assertEqual(2, tur1.posisjoner[0].x_koordinat)
+        self.assertEqual(3, tur1.posisjoner[0].y_koordinat)
+        self.assertEqual(2, tur1.posisjoner[0].hoyde)
+        tur1.add_posisjon(punkt2)
+        self.assertEqual(2, len(tur1.posisjoner))
+        self.assertEqual(4, tur1.posisjoner[1].x_koordinat)
+        self.assertEqual(4, tur1.posisjoner[1].y_koordinat)
+        self.assertEqual(7, tur1.posisjoner[1].hoyde)
+
+    def test_add_posisjon_koordinater(self):
+        tur1 = Tur('Tur1', 0, 15)
+        tur1.add_posisjon_koordinater(2, 3, 2)
+        self.assertEqual(1, len(tur1.posisjoner))
+        self.assertEqual(2, tur1.posisjoner[0].x_koordinat)
+        self.assertEqual(3, tur1.posisjoner[0].y_koordinat)
+        self.assertEqual(2, tur1.posisjoner[0].hoyde)
+        tur1.add_posisjon_koordinater(4, 4, 7)
+        self.assertEqual(2, len(tur1.posisjoner))
+        self.assertEqual(4, tur1.posisjoner[1].x_koordinat)
+        self.assertEqual(4, tur1.posisjoner[1].y_koordinat)
+        self.assertEqual(7, tur1.posisjoner[1].hoyde)
+
+    def test_hoydemeter(self):
+        tur1 = Tur('Tur', 0, 15)
+        tur2 = Tur('Tur', 4, 6)
+        punkt1 = Posisjon(2, 3, 2)
+        punkt2 = Posisjon(20, 5, 20)
+        punkt3 = Posisjon(18, 13, 22)
+        punkt4 = Posisjon(2, 3, 2)
+        punkt5 = Posisjon(5, 3, 10)
+        punkt6 = Posisjon(15, 5, 18)
+        punkt7 = Posisjon(12, 15, 12)
+        tur1.add_posisjon(punkt1)
+        tur1.add_posisjon(punkt2)
+        tur1.add_posisjon(punkt3)
+        tur1.add_posisjon(punkt4)
+        self.assertEqual(40, tur1.hoydemeter())
+        tur2.add_posisjon(punkt5)
+        tur2.add_posisjon(punkt6)
+        tur2.add_posisjon(punkt7)
+        self.assertEqual(14, tur2.hoydemeter())
+
+    def test_er_rundtur(self):
+        tur1 = Tur('Tur', 0, 15)
+        tur2 = Tur('Tur', 4, 6)
+        punkt1 = Posisjon(2, 3, 2)
+        punkt2 = Posisjon(20, 5, 20)
+        punkt3 = Posisjon(18, 13, 22)
+        punkt4 = Posisjon(2, 3, 2)
+        punkt5 = Posisjon(5, 3, 10)
+        punkt6 = Posisjon(15, 5, 18)
+        punkt7 = Posisjon(12, 15, 12)
+        tur1.add_posisjon(punkt1)
+        tur1.add_posisjon(punkt2)
+        tur1.add_posisjon(punkt3)
+        tur1.add_posisjon(punkt4)
+        tur2.add_posisjon(punkt5)
+        tur2.add_posisjon(punkt6)
+        tur2.add_posisjon(punkt7)
+        self.assertTrue(tur1.er_rundtur())
+        self.assertFalse(tur2.er_rundtur())
 
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
-
-"""
-Skriv enhetstester for egenskapen slutt-tidspunkt og metodene som er oppgitt over. 
-Lag turer med eksempelverdiene oppgitt over for å teste «hoydemeter» og «er_rundtur» metodene. 
-Du kan gjerne lage flere turer også. 
-Testen for slutt-tidspunkt skal sjekke at du får en exception hvis du prøver å lage en tur som slutter før den starter.
-"""
--- tur_fixed.py
+++ tur_fixed.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Posisjon:
     def __init__(self, x_koordinat, y_koordinat, hoyde):
         self.x_koordinat = x_koordinat
@@ -31,29 +32,37 @@ class Tur:
         self.sluttidspunkt = sluttidspunkt
         self.posisjoner = list()
 
+    # La til en property for sluttidspunktet, samt en sluttidspunkt.setter
+    # Her sjekkes det at sluttidspunktet er etter starttidspunktet
+    @property
+    def sluttidspunkt(self):
+        return self.__sluttidspunkt
+
+    @sluttidspunkt.setter
+    def sluttidspunkt(self, ny_slutt):
+        if ny_slutt > self.starttidspunkt:
+            self.__sluttidspunkt = ny_slutt
+        else:
+            raise ValueError('Du kan ikke avslutte turen før den har begynt, slask!')
+
     def add_posisjon(self, posisjon):
         self.posisjoner.append(posisjon)
 
+    # Endret fra (self, x_koordinat, x_koordinat, hoyde)
+    # til (self, x_koordinat, y_koordinat, hoyde)
     def add_posisjon_koordinater(self, x_koordinat, y_koordinat, hoyde):
-        self.posisjoner.append(Posisjon(x_koordinat, x_koordinat, hoyde))
+        self.posisjoner.append(Posisjon(x_koordinat, y_koordinat, hoyde))
 
+    # Endret range fra range(len(self.posisjoner)) til range(len(self.posisjoner)-1)
+    # Endret fra resultat += self.posisjoner[i-1].hoydeforskjell(self.posisjoner[i])
+    # Til resultat += self.posisjoner[i].hoydeforskjell(self.posisjoner[i+1])
     def hoydemeter(self):
         resultat = 0.0
-        for i in range(len(self.posisjoner)):
-            resultat += self.posisjoner[i-1].hoydeforskjell(self.posisjoner[i])
+        for i in range(len(self.posisjoner)-1):
+            resultat += self.posisjoner[i].hoydeforskjell(self.posisjoner[i+1])
         return resultat
 
     def er_rundtur(self):
         if self.posisjoner[0] == self.posisjoner[-1]:
             return True
         return False
-
-
-    @property
-    def navn(self):
-        return self.navn
-
-    @navn.setter
-    def navn(self, verdi):
-        if type(verdi) != str:
-            raise TypeError("This name has to be type str")
